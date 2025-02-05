47023b34ecdfa13c307fd98a490cdad9f624dfd2
miguelperales10 <miguel.perales@uclm.es>
2025-02-05 13:51:39 +0100

    Update and rename tto_QRT.m to CRT_treatment_sim.m

--- humans_virt_trial/CRT_treatment_sim.m
+++ humans_virt_trial/CRT_treatment_sim.m
@@ -1,4 +1,4 @@
-function [t,poblaciones] = tto_QRT(p_in,parametros,RT_dosis,QT_dosis,t_despues_tto)
+function [t,poblaciones] = CRT_treatment_sim(p_in,parametros,RT_dosis,QT_dosis,t_despues_tto)
 
 % Vector de tiempo
 t=[]; 
@@ -7,8 +7,8 @@ t=[];
 primera_RT= min(RT_dosis);
 primera_QT= min(QT_dosis);
 idx_tto=1;
-combined_tto=union(RT_dosis,QT_dosis);
-n_tto=length(combined_tto);
+combined_treat=union(RT_dosis,QT_dosis);
+n_tto=length(combined_treat);
 
 %% All parameters. Some of them are not used in the ODE. 
 rho_S = parametros(1);
@@ -34,7 +34,7 @@ delta_ED = parametros(15);
 t_in = 0; 
 t_f = min(primera_RT,primera_QT); % First RT/drug administration
 
-[t1,y1] = ode45(@(t,y)ecuaciones_QRT(t,y,parametros),t_in: 0.001 : t_f, p_in');
+[t1,y1] = ode45(@(t,y)eqs_CRT(t,y,parametros),t_in: 0.001 : t_f, p_in');
 
 t=[t;t1];
 S = y1(:,1); % sensitive
@@ -52,7 +52,7 @@ p_in = [S(end), PI(end), P(end), R(end), Q(end), D(end), E_D(end)];
 
 for i=1:n_tto
 
-    t_tto=combined_tto(idx_tto);
+    t_tto=combined_treat(idx_tto);
 
             if any(RT_dosis==t_tto) 
 
@@ -75,10 +75,10 @@ for i=1:n_tto
             if idx_tto<n_tto
             t_in = t_tto; 
             idx_tto=idx_tto+1;
-            t_tto=combined_tto(idx_tto);
+            t_tto=combined_treat(idx_tto);
             t_f = t_tto; 
             
-            [t1,y1] = ode45(@(t,y)ecuaciones_QRT(t,y,parametros),t_in: 0.001 : t_f, p_in');
+            [t1,y1] = ode45(@(t,y)eqs_CRT(t,y,parametros),t_in: 0.001 : t_f, p_in');
             t=[t;t1];
             S = [S;y1(:,1)]; % sensitive
             PI = [PI;y1(:,2)]; % persisters PI
@@ -96,7 +96,7 @@ end
 t_in = t_f; 
 t_f = t_in+t_despues_tto; 
 
-[t1,y1] = ode45(@(t,y)ecuaciones_QRT(t,y,parametros),t_in: 0.001 : t_f, p_in');
+[t1,y1] = ode45(@(t,y)eqs_CRT(t,y,parametros),t_in: 0.001 : t_f, p_in');
 t=[t;t1];
 S = [S;y1(:,1)]; % sensitive
 PI = [PI;y1(:,2)]; % persisters PI
