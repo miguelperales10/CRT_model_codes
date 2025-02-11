24e215fa6a9a3f503fe34ef7417c22a9a6b907d2
miguel_perales <miguel.perales@uclm.es>
2025-02-11 19:05:20 +0100

    Update schedule_treatment.m

--- qrt_mice/schedule_treatment.m
+++ qrt_mice/schedule_treatment.m
@@ -29,19 +29,19 @@ if num_treatment == tto_control
 
 
     elseif num_treatment == tto_RT_X1
-    RT_tto=[1,2,3];
+    RT_tto=[1,2,3] + 30;
     QT_tto=199;  % first row: days - NO TMZ 
             %3,3,3];  % second row: number of dosis
     t_despues_tto =  200;
     
     elseif num_treatment == tto_RT_X4
-    RT_tto=[1,5,9];
+    RT_tto=[1,5,9] + 30;
     QT_tto=199;  % first row: days - NO TMZ 
             %3,3,3];  % second row: number of dosis
     t_despues_tto =  200;
     
     elseif num_treatment == tto_RT_X7
-    RT_tto=[1,8,15];
+    RT_tto=[1,8,15] + 30;
     QT_tto=199;  % first row: days - NO TMZ 
             %3,3,3];  % second row: number of dosis
     t_despues_tto =  200;
@@ -49,25 +49,25 @@ if num_treatment == tto_control
     
     
     elseif num_treatment == tto_TMZ_X1
-    QT_tto=[1,1.2,1.4,2,2.2,2.4,3,3.2,3.4];  % first row: days - NO RT
+    QT_tto=[1,1.2,1.4,2,2.2,2.4,3,3.2,3.4] + 7;  % first row: days - NO RT
            % 3,3,3];  % second row: number of dosis
     RT_tto=199;
     t_despues_tto =  200;
     
     elseif num_treatment == tto_TMZ_X4
-    QT_tto=[1,1.2,1.4,5,5.2,5.4,9,9.2,9.4];  % first row: days - NO RT
+    QT_tto=[1,1.2,1.4,5,5.2,5.4,9,9.2,9.4] + 7;  % first row: days - NO RT
             %3,3,3];  % second row: number of dosis
     RT_tto=199;
     t_despues_tto =  200;
         
     elseif num_treatment == tto_TMZ_X7
-    QT_tto=[1,1.2,1.4,8,8.2,8.4,15,15.2,15.4];  % first row: days - NO RT
+    QT_tto=[1,1.2,1.4,8,8.2,8.4,15,15.2,15.4] + 7;  % first row: days - NO RT
             %3,3,3];  % second row: number of dosis
     RT_tto= 199;
     t_despues_tto =  200;
 
     elseif num_treatment == tto_TMZ_X13
-    QT_tto=[1,1.2,1.4,14,14.2,14.4,27,27.2,27.4];  % first row: days - NO RT
+    QT_tto=[1,1.2,1.4,14,14.2,14.4,27,27.2,27.4] + 7;  % first row: days - NO RT
             %3,3,3];  % second row: number of dosis
     RT_tto= 199;
     t_despues_tto =  200;
