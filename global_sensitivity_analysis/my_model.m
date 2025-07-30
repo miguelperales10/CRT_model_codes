ba4e24c07070a08aefe646249ac88c256252b9d2
miguel_perales <miguel.perales@uclm.es>
2025-07-30 13:30:05 +0200

    Update my_model.m

--- global_sensitivity_analysis/my_model.m
+++ global_sensitivity_analysis/my_model.m
@@ -14,19 +14,19 @@ treat_start = 4;
 
 if delta_start >=0 % If RT starts before
  RT_treat = [treat_start, treat_start+delta_RT, treat_start+2*delta_RT];
- QT_treat = [treat_start+delta_start, treat_start+delta_start+delta_TMZ, treat_start+delta_start+2*delta_TMZ];
- QT_treat = [QT_treat, QT_treat+0.2, QT_treat+0.4]; QT_treat = sort(QT_treat);
+ CT_treat = [treat_start+delta_start, treat_start+delta_start+delta_TMZ, treat_start+delta_start+2*delta_TMZ];
+ CT_treat = [CT_treat, CT_treat+0.2, CT_treat+0.4]; CT_treat = sort(CT_treat);
 else               % If CT starts before
  RT_treat = [treat_start-delta_start, treat_start-delta_start+delta_RT, treat_start-delta_start+2*delta_RT];
- QT_treat = [treat_start, treat_start+delta_TMZ, treat_start+2*delta_TMZ];
- QT_treat = [QT_treat, QT_treat+0.2, QT_treat+0.4]; QT_treat = sort(QT_treat);
+ CT_treat = [treat_start, treat_start+delta_TMZ, treat_start+2*delta_TMZ];
+ CT_treat = [CT_treat, CT_treat+0.2, CT_treat+0.4]; CT_treat = sort(CT_treat);
 end
 
 % Simulation after treatment finishes
  t_after_treat =  200;
 
 % Solve ODEs
-[t,pops] = simul_combined_CRT(p_in,param,RT_treat,QT_treat,t_after_treat);
+[t,pops] = simul_combined_CRT(p_in,param,RT_treat,CT_treat,t_after_treat);
 
 % Update subpops
 S = pops(:,1); PI = pops(:,2); P = pops(:,3); R = pops(:,4);
@@ -38,7 +38,7 @@ IVIS_death = 0.3e+08;
 r = find (total >= IVIS_death, 1);
 % OS too large are truncated
 if isempty(r) 
-    OS = t_after_treat + QT_treat(end);
+    OS = t_after_treat + CT_treat(end);
 else
 OS = r*t_span_dif;
 
